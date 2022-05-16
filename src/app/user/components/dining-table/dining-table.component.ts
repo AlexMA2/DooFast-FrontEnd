5ca1925072d42c4d6e49e657d8a11b94f2131737
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-05-16 12:43:18 -0500

    Added color and styles

--- src/app/user/components/dining-table/dining-table.component.ts
+++ src/app/user/components/dining-table/dining-table.component.ts
@@ -8,12 +8,36 @@ import { EMPTY, UNATTENDED, WAITING, SERVED } from '../../constants/dining-table
 })
 export class DiningTableComponent implements OnInit {
 
+  WAITING = WAITING; // Angular cannot use the imported constants from constants.ts in HTML
   number : number = 1;
   state : string = EMPTY;
+  time: number = 0
+
+  display : string = "00m 00s "
+  interval : any;
+
+ startTimer() {
+    console.log("=====>");
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time++;
+      } else {
+        this.time++;
+      }
+      this.display=this.transform( this.time)
+    }, 1000);
+  }
+  transform(value: number): string {
+       const minutes: number = Math.floor(value / 60);
+       return minutes + 'm ' + (value - minutes * 60) + 's';
+  }
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
-
 }
