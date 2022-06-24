f1ff30e66a7bd47fab231a3a60478418a4d9bfed
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-06-24 18:56:01 -0500

    Push forzoso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiningTableContainerComponent } from './user/pages/dining-table-container/dining-table-container.component';
-import { PendingOrdersListComponent } from './user/pages/pending-orders-list/pending-orders-list.component';
 import { TakeOrderComponent } from './user/pages/take-order/take-order.component';
+import { PendingOrdersListComponent } from './user/pages/pending-orders-list/pending-orders-list.component';
 
 const WaitressRoutes: Routes = [
   {
