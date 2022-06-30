0cb123c8e78bb00e883f175a01d004404cdea4ba
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-06-30 16:20:21 -0500

    Add login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(email: string, password: string) {
+    return this.http.post('https://0a2c-190-237-151-210.sa.ngrok.io/api/Auth', {
+      correo: 'string@gmail.com',
+      contrasenia: '123',
+    });
+  }
 }
