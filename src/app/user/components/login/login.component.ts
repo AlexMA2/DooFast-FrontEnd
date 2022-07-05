411a92919a9cee06327b0c55c9bfc64d5a7df99b
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-07-05 12:56:41 -0500

    Added validations to login and styles corrected

--- src/app/user/components/login/login.component.ts
+++ src/app/user/components/login/login.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,45 +16,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   formLogin!: FormGroup;
 
+  valids: any = {
+    correo: false,
+    password: false,
+  };
+
   constructor(
     formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router
   ) {
     this.formLogin = formBuilder.group({
-      correo: ['', Validators.required],
-      password: ['', Validators.required],
+      correo: ['', Validators.email],
+      password: ['', Validators.minLength(8)],
     });
   }
 
+  validateInput(field: string) {
+    this.valids[field] =
+      this.formLogin.get(field)?.value.length &&
+      this.formLogin.get(field)?.invalid &&
+      (this.formLogin.get(field)?.dirty || this.formLogin.get(field)?.touched);
+  }
+
   Login() {
-    console.log(
-      this.formLogin.get('correo')?.value,
-      this.formLogin.get('password')?.value
-    );
-    this.authService
-      .login(
-        this.formLogin.get('correo')?.value,
-        this.formLogin.get('password')?.value
-      )
-      .subscribe(
-        (data: any) => {
-          console.log(data);
-          switch (data[0].rol) {
-            case 'Administrador':
-              this.router.navigate(['admin']);
-              break;
-            case 'Mozo':
-              this.router.navigate(['waitress']);
-              break;
-            case 'Cocina':
-              this.router.navigate(['cocina']);
-              break;
+    if (!this.formLogin.invalid) {
+      this.authService
+        .login(
+          this.formLogin.get('correo')?.value,
+          this.formLogin.get('password')?.value
+        )
+        .subscribe(
+          (data: any) => {
+            console.log(data);
+            switch (data[0].rol) {
+              case 'Administrador':
+                this.router.navigate(['admin']);
+                break;
+              case 'Mozo':
+                this.router.navigate(['waitress']);
+                break;
+              case 'Cocina':
+                this.router.navigate(['cocina']);
+                break;
+            }
+          },
+          (error) => {
+            alert(error.error.message);
           }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+        );
+    }
   }
 }
