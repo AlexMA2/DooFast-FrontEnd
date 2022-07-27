3325765c83bcea49d326ae8cc9da48019082f9c2
Ol-i-v-e-r <shieldraw2@gmail.com>
2022-07-26 22:04:29 -0500

    se puede eliminar empleados

--- src/app/admin/pages/employees/employees.component.ts
+++ src/app/admin/pages/employees/employees.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UserData } from 'src/app/models/UserData';
 import { UserDataService } from 'src/app/services/userData/user-data.service';
+import { MatButtonModule } from '@angular/material/button';
 
 /*const ELEMENT_DATA: UserData[] = [
   {idUsuario: 1, nombreUsuario: 'Joel', nroCelular: '1111', correo: 'gmail', rol: 'mesero', idRestaurante: 1},
@@ -25,7 +26,7 @@ export class EmployeesComponent implements OnInit {
 
   ngOnInit(): void {
     this.displayedColumns = ['idUsuario', 'nombreUsuario', 'nroCelular', 
-    'correo', 'rol', 'local'];
+    'correo', 'rol', 'local', 'delete'];
     this.getAllUsers();
   }
 
@@ -37,6 +38,13 @@ export class EmployeesComponent implements OnInit {
         },
         err => console.error(err)
       );
+  }
+
+  deleteProduct(u: UserData) {
+    this.userDataService.deleteUser(u.idUsuario).subscribe((data) => {
+      console.log(data);
+      this.getAllUsers();
+    });
   }
 
 }
