d15aaa3a1fa26cd6d44e225e805da1c0c2df9d8a
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-08-05 12:43:45 -0500

    Added guards to routes

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiningTableContainerComponent } from './pages/dining-table-container/dining-table-container.component';
 import { TakeOrderComponent } from './pages/take-order/take-order.component';
+import { RecordPaymentComponent } from './pages/record-payment/record-payment.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'take-order/:id',
     component: TakeOrderComponent,
+  },
+  {
+    path: 'pay-order/:id',
+    component: RecordPaymentComponent,
   },
 ];
 
