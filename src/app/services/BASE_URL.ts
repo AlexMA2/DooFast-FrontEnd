3d9126b78217599c42876309e4cb3d020c4b5b06
Alex <58560420+AlexMA2@users.noreply.github.com>
2022-08-19 11:00:07 -0500

    BASE URL has been changed

--- src/app/services/BASE_URL.ts
+++ src/app/services/BASE_URL.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://0b91-190-237-151-38.sa.ngrok.io/';
+export const BASE_URL = 'https://735a-190-237-151-38.sa.ngrok.io';
 export const LOCAL_URL = 'https://localhost:44301/';
